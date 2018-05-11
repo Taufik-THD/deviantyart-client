fe988530bc06920c75123e5a7cc1d7deb0019fd5
Taufik Hidayat <ovick_kd@yahoo.com>
2018-05-11 15:40:29 +0700

    done

--- profile.js
+++ profile.js
@@ -1,15 +1,15 @@
 const profile = new Vue({
     el: "#profile",
     data:{
-        Pictures: []
+      Pictures: []
     },
     created: function () {
         console.log('Created');
-        this.getImageById('12345');
+        this.getImages();
     },
-    methods:{ 
+    methods:{
         getImageById(userId){
-            axios.get(`http://localhost:9000/pictures/${userId}`)
+            axios.get(`http://localhost:3000/pictures/${userId}`)
                 .then(({data})=>{
                     this.Pictures = data;
                 })
@@ -18,14 +18,23 @@ const profile = new Vue({
             })
         },
         addLikes(userId, index){
-            axios.put(`http://localhost:9000/pictures/${userId}`)
+            axios.put(`http://localhost:3000/pictures/${userId}`)
             .then(({data})=>{
                 this.Pictures[index].likes += 1;
             })
             .catch(err=>{
                 return err
             })
+        },
+        getImages(){
+          axios.post(`http://localhost:3000/user/images`,{token:localStorage.token})
+          .then(images=>{
+            this.Pictures=images.data.images
+            console.log(images.data.images);
+          })
+          .catch(err=>{
+            return err
+          })
         }
     }
   })
-  
